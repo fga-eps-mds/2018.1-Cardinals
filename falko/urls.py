8487ce18aac33511b995b0f1549562a3fbbef51a
Lucas Costa <lucascostaa73@gmail.com>
2018-03-27 00:00:31 -0300

    Revert "Feature view dev"

--- falko/urls.py
+++ falko/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-from dev import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^dev', include('dev.urls'))
 ]
