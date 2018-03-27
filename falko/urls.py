7a5a8197a0d1a025eac04c7cd07a01c4f92a1065
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-03-27 00:14:44 -0300

    merge feature_view_new with develop

--- falko/urls.py
+++ falko/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from dev import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
