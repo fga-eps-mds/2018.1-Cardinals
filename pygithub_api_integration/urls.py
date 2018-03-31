adc5511f0f00fd5fdc598893bac8459dd788eb13
mateusas3s <mateusaugusto-2009@hotmail.com>
2018-03-31 20:13:33 -0300

    created function for print project contributors

--- pygithub_api_integration/urls.py
+++ pygithub_api_integration/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from pygithub_api_integration import views
 
 urlpatterns = [
-    url(r'firstTest/', views.getPygithub, name='firstTest'),
-    url(r'^getRepo/', views.getRepository, name='getRepo')
+    url(r'firstTest/$', views.getPygithub, name='firstTest'),
+    url(r'getContributors/$', views.getContributors, name='getContributors')
 ]
--- pygithub_api_integration/views.py
+++ pygithub_api_integration/views.py
@@ -1,25 +1,23 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 from github import Github
 
 
 def getPygithub(request):
-    # First create a Github instance:
+    git = Github("Mateusas3s", "123qaz.;/")
+    git = Github("9a9d59fc51a9f25ade73f785214ba0f935749ee6")
+    user = git.get_user()
+    repos = user.get_repos()
 
-    # using username and password
-    g = Github("Mateusas3s", "123qaz.;/")
-
-    # or using an access token
-    g = Github("9a9d59fc51a9f25ade73f785214ba0f935749ee6")
-
-    # Then play with your Github objects:
-    for repo in g.get_user().get_repos():
-        return HttpResponse(repo.name + '</br>')
+    return render(request, 'repos.html',
+                  {"repos": repos})
 
 
-def getRepository(request):
+def getContributors(request):
 
-    g = Github("9a9d59fc51a9f25ade73f785214ba0f935749ee6")
-    org = g.get_organization('https://github.com/fga-gpp-mds')
-    repo = org.get_repo('https://github.com/fga-gpp-mds/2018.1-Cardinals')
-    return HttpResponse(repo)
+    git = Github("9a9d59fc51a9f25ade73f785214ba0f935749ee6")
+    org = git.get_organization('fga-gpp-mds')
+    repo = org.get_repo('2018.1-Cardinals')
+    contributors = repo.get_contributors()
+    return render(request, 'contributors.html',
+                  {"contributors": contributors})
