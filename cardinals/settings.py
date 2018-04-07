1bfbe851b7095d83a8d856542ce3354efe1e94b8
amandabezerra <amandabs_contato@hotmail.com>
2018-04-07 01:04:13 -0300

    Fix static files error

--- cardinals/settings.py
+++ cardinals/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
