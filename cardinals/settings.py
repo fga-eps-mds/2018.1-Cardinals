fa3cb57a9c44a7b3235d41d3d44df54a4f0d7a7f
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-04-10 18:01:44 -0300

    settings.py configured to accept bootstrap

--- cardinals/settings.py
+++ cardinals/settings.py
@@ -123,4 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = ''
+
+STATICFILES_DIRS = (os.path.join('static'), )
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+    'sass_processor.finders.CssFinder',
+]
+
 STATIC_URL = '/static/'
