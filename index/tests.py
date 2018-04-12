1e35a43b7733797406ff8b429807c462ac3cac8c
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-04-12 18:08:10 -0300

    Updating .gitignore and test for index
    Signed-off-by: Mateus Augusto <mateusaugusto-2009@hotmail.com>
    Signed-off-by: Joao Pedro <jpmartins201@live.com>

--- index/tests.py
+++ index/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_template_index(self):
+        self.assertTemplateUsed('templates/index.html')
+
+    def test_searchRepository(self):
+        data = {
+            'repository': 'fga-gpp-mds/2018.1-Cardinals',
+        }
+        response = self.c.post('/pyGithub/getContributors/', data)
+        self.assertEquals(200, response.status_code)
