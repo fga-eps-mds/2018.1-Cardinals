e3ed721011dd4799028cfadbf35e547adc17832f
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-04-11 22:08:19 -0300

    create view user_commits

--- user_commits/views.py
+++ user_commits/views.py
@@ -1,22 +1,6 @@
-from github import Github
-from datetime import datetime
+from django.shortcuts import render
 
-
-g = Github("mdscardinals", "(cardinals1)")
-
-org = g.get_organization('fga-gpp-mds')
-repo = org.get_repo('2018.1-Cardinals')
-
-datacriacao = datetime(2018,4,1)
-datafechamento = datetime(2018,4,4)
-lista = []
-
-estado="closed"
-
-
-stes = repo.get_stats_contributors()
-
-
-for i in stes:
-        lista = i.author, i.total
-        print(lista)
+def user_commits(request):
+    
+   
+    return render(request, 'user_commits.html')
