5bfbce1f2a357825f83b3830d62c8b0f1d894b29
João Pedro <jpmartins201@live.com>
2018-04-11 22:08:19 -0300

    Update view user_commits

--- user_commits/views.py
+++ user_commits/views.py
@@ -1,22 +1,17 @@
 from github import Github
+from django.shortcuts import render
 from datetime import datetime
 
+def user_commits(request):
+    
+    g = Github("mdscardinals", "(cardinals1)")
 
-g = Github("mdscardinals", "(cardinals1)")
+    org = g.get_organization('fga-gpp-mds')
+    repo = org.get_repo('2018.1-Cardinals')      
 
-org = g.get_organization('fga-gpp-mds')
-repo = org.get_repo('2018.1-Cardinals')
+    allCommits = repo.get_stats_contributors()
 
-datacriacao = datetime(2018,4,1)
-datafechamento = datetime(2018,4,4)
-lista = []
+    
 
-estado="closed"
+    return render(request, 'user_commits.html', {"allCommits" : allCommits})
 
-
-stes = repo.get_stats_contributors()
-
-
-for i in stes:
-        lista = i.author, i.total
-        print(lista)
