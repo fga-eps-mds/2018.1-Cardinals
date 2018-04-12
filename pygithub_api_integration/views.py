9289d56543ce5938c3cf093bd5665c4602fa1c81
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-04-12 13:38:14 -0300

    created repository search

--- pygithub_api_integration/views.py
+++ pygithub_api_integration/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import render
 from github import Github
-# from github import GithubException
+from index.views import searchRepository
+
 
 username = "mdscardinals"
 password = "(cardinals1)"
-repo = '2018.1-Cardinals'
-
-'''
-def request_oauth_token(username, password, repo):
-
-    try:
-        git = Github(username, password)
-        user = git.get_user()
-        auth = user.create_authorization(scopes="public_repo", note=repo)
-        return auth.token
-    except GithubException as e:
-        msg_error = "ERROR: " + str(e)
-        return msg_error
-'''
+# repo = '2018.1-Cardinals'
 
 
 def getRepo(request):
 
-    # token = request_oauth_token(username, password, repo)
-
     git = Github(username, password)
-    # git = Github(token)
     user = git.get_user()
     repos = user.get_repos()
 
@@ -35,10 +20,10 @@ def getRepo(request):
 
 def getContributors(request):
 
-    # token = request_oauth_token
-    git = Github(username, password)
-    org = git.get_organization('fga-gpp-mds')
-    repo = org.get_repo('2018.1-Cardinals')
+    repo_name = searchRepository(request)
+
+    git = Github()
+    repo = git.get_repo(repo_name)
     contributors = repo.get_contributors()
 
     return render(request, 'contributors.html',
