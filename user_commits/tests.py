f814b9e16140e722f21a93270f25ad270fb41f2e
Joao Pedro <jpmartins201@live.com>
2018-04-13 15:16:22 -0300

    Adding tests for user_commits

--- user_commits/tests.py
+++ user_commits/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestCommits(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_user_commits(self):
+        url = reverse('user_commits')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, 'allCommits')
