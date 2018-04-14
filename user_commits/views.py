2a7546d849452f4ac13adef88f50ff1cec1cfdee
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-04-14 15:22:38 -0300

    modified view user commits

--- user_commits/views.py
+++ user_commits/views.py
@@ -1,11 +1,27 @@
 from github import Github
 from django.shortcuts import render
+from index.views import searchRepository
+from oauth.credentials import get_credentials
 from datetime import datetime
 
-def user_commits(request):
 
-    g = Github("mdscardinals", "(cardinals1)")
-    org = g.get_organization('fga-gpp-mds')
-    repo = org.get_repo('2018.1-Cardinals')
+username, password = get_credentials()
+repo = '2018.1-Cardinals'
+
+
+def user_commits(request):
     allCommits = repo.get_stats_contributors()
-    return render(request, 'user_commits.html', {"allCommits" : allCommits})
+    return allCommits
+
+
+def getRepoInfo(request):
+
+    repo_name = searchRepository(request)
+
+    git = Github()
+    repo = git.get_repo(repo_name)
+
+    allcommits = getAllCommits(repo)
+    
+    return render(request, 'user_commits.html',
+                  {"allcommits ": allcommits})
