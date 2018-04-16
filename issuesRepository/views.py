c705e56b978fe0d348b8861177431cf5895fe47e
Matheus Gomes <matheusgomesf01@gmail.com>
2018-04-15 23:53:19 -0300

    refatorando codigo

--- issuesRepository/views.py
+++ issuesRepository/views.py
@@ -5,17 +5,23 @@ from index.views import searchRepository
 username = "mdscardinals"
 password = "(cardinals1)"
 
-
 def getIssues(request):
     return render(request, 'issues.html')
 
 def getResultIssues(request):
-
     repo_name = searchRepository(request)
-
+    status = searchState(request)
     git = Github(username, password)
     repo = git.get_repo(repo_name)
     issues = repo.get_issues()
-
     return render(request, 'resultsIssues.html',
-                  {"issues": issues})
+                  {"issues": issues, "status" : status})
+
+def searchState(request):
+    if request.method == 'GET':
+        repository = request.GET['repository2']
+
+        return repository
+    else:
+        message = 'oi'
+        return message
