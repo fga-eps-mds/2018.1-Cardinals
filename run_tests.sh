c46c1d3b3bbdedc5e32e7bfa1f3a593ca8b1c6f1
amandabezerra <amandabs_contato@hotmail.com>
2018-05-06 12:32:43 -0300

    Adjust tests and analysis configurations

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,18 @@
 
 python manage.py makemigrations
 python manage.py migrate
-
-# Test related tasks
 coverage run --source='.' manage.py test
+
+RESULT=$?
+if [ ${RESULT} != "0" ]; then
+	echo -e "\nOh no, some test failed!"
+	exit ${RESULT}
+fi
+
+echo -e "\nGreat, all tests succeed!\n"
+
 coverage report
+
 codecov -t 4bbc492c-d908-4f02-8595-8a510df37ef3
+
+exit 0
