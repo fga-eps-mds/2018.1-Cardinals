60db6ae548e606c35f47455c2725458b81c00603
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-05-06 16:12:52 -0300

    geting contributors and commits number

--- ranking_commiters/views.py
+++ ranking_commiters/views.py
@@ -11,7 +11,57 @@ repo = org.get_repo('2018.1-Cardinals')
 
 def getRankingCommiters(request):
 
-    commits_user = repo.get_stats_contributors()
+    rankingCommiters = scoreContributor()
 
     return render(request, 'rankingCommiters.html',
-                  {"commits_user": commits_user})
+                  {"rankingCommiters": rankingCommiters})
+
+
+def getStatsContributors():
+
+    contributors = {}
+
+    stats_contributors = repo.get_stats_contributors()
+
+    for sc in stats_contributors:
+        contributors[sc.author.id] = {"name": sc.author.name,
+                                      "commits": sc.total}
+
+    return contributors
+
+
+def getIssuesCreatedFor(contributor):
+
+    issues_created = repo.get_issues(creator=contributor)
+    num_issues_created = 0
+
+    for i in issues_created:
+        num_issues_created += 1
+
+    return num_issues_created
+
+
+def getIssuesClosedFor(contributor):
+
+    issues_closed = repo.get_issues(state="closed")
+
+    for i in issues_closed:
+        if i.closed_by == contributor:
+            issues_closed_for_contributor = issues_closed
+
+    return issues_closed_for_contributor
+
+
+def scoreContributor():
+
+    contributors_ranked = []
+
+    stats_contributors = getStatsContributors()
+
+    for i, sc in stats_contributors:
+        contributors_ranked[i] = {"nome": sc.author.name,
+                                  "commits": sc.total,
+                                  "issues_created": getIssuesCreatedFor(sc.author),
+                                  "issue_closed": getIssuesClosedFor(sc.author)
+                                  }
+    return contributors_ranked
