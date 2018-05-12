34f24a1576467c51772746610db2163069febc54
Lorrany Azevedo <lorrany90@gmail.com>
2018-05-12 15:51:12 -0300

    Refactoring old django urls to new django paths
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Marlon Mendes <marlonciriatico@gmail.com>

--- cardinals/urls.py
+++ cardinals/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path
+
 
 urlpatterns = [
+    path('', include('index.urls'), name='index'),
+
     url(r'^searchDocs/', include('searchDocs.urls')),
-    url(r'^$', include('index.urls'), name='index'),
     url(r'^dev/', include('dev.urls'), name='dev'),
-    url(r'^pyGithub/', include('pygithub_api_integration.urls')),
+    path('pyGithub/', include('pygithub_api_integration.urls')),
     url(r'^user_commits/', include('user_commits.urls')),
     url(r'^issueRepo/', include('issuesRepository.urls')),
 ]
