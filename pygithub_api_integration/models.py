7b88b627d075d9a6abd4e12f670fa05b016f4616
mateusas3s <mateusaugusto-2009@hotmail.com>
2018-05-12 15:11:50 -0300

    Creating models Co-authored_by: jpmartins201 <jpmartins201@live.com> Co-authored_by: MiguelNery <nery.mik@gmail.com>

--- pygithub_api_integration/models.py
+++ pygithub_api_integration/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+
+    full_name = models.CharField(primary_key=True, max_length=255)
+    name = models.CharField(max_length=255, null=False)
+
+
+class Commit(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    created_at = models.DateTimeField()
+    id_repository = models.ForeignKey('Repository',
+                                      on_delete=models.CASCADE)
+
+
+class Contributor(models.Model):
+
+    id = models.BigIntegerField(primary_key=True)
+    username = models.CharField(max_length=255, null=False)
+    repositories = models.ManyToManyField(Repository)
+    commits = models.ManyToManyField(Commit)
+
+
+class ContributingWeek(models.Model):
+
+    line_add = models.IntegerField()
+    line_deletion = models.IntegerField()
+    total_commits = models.IntegerField()
+    id_contributor = models.ForeignKey('Contributor',
+                                       on_delete=models.CASCADE)
+
+
+class Issue(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    created_by = models.BigIntegerField()  # ID of creator contributor
+    closed_by = models.BigIntegerField()  # ID of closing contributor
+    is_closed = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    closed_at = models.DateTimeField()
+    id_repository = models.ForeignKey('Repository',
+                                      on_delete=models.CASCADE)
