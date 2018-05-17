35834172614fb1217cf8a9455ec9ddefb4c57139
Lorrany Azevedo <lorrany90@gmail.com>
2018-05-17 15:37:05 -0300

    Test get_contributors_name

--- pygithub_api_integration/tests.py
+++ pygithub_api_integration/tests.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class RepoInfoTests(TestCase):
 
+
     def test_get_repo_name(self):
 
         organization = 'fga-gpp-mds'
@@ -29,7 +30,30 @@ class RepoInfoTests(TestCase):
         
         self.assertEquals(repo_name, response_repo_name)
 
-    def test_get_contributors(self):
+
+    def test_get_contributors_name(self):
+        organization = 'fga-gpp-mds'
+        repo_name = '2018.1-Cardinals'
+        repo_path = organization + '/' + repo_name
+
+        url = reverse('getRepoInfo')
+        content = {'repository': repo_path}
+        response = self.client.post(url, content)
+
+        contributors = response.context['contributors']
+
+        contributors_name_expected = ['Amanda Bezerra', 'Mateus Augusto Sousa e Silva', 'Marlon Mendes', 'Guilherme da Luz',
+                                      'Lucas Costa', 'Gustavo Duarte Moreira', 'Matheus Gomes', 'Lorrany Azevedo',
+                                      'João Pedro', 'Mik', 'Victor Moura' ]
+
+        contributors_name_expected = set(contributors_name_expected)
+
+        contributors_name = set([contributor.name for contributor in contributors])
+
+        self.assertEquals(contributors_name, contributors_name_expected)
+
+
+    def test_get_contributors_login(self):
 
         organization = 'fga-gpp-mds'
         repo_name = '2018.1-Cardinals'
@@ -46,7 +70,9 @@ class RepoInfoTests(TestCase):
                                        'lucasca73', 'victorcmoura', 'MiguelNery']
 
         contributors_login_expected = set(contributors_login_expected)
+
         contributors_login = set([contributor.login for contributor in contributors])
 
         self.assertEquals(contributors_login, contributors_login_expected)
+
 
