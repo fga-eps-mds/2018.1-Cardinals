71c2153c6283cd08bbf430faf1f9e3eaf09378d0
Marlon Mendes <marlonciriatico@gmail.com>
2018-05-24 18:53:16 -0300

    Getting pull requests from pygithub API
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Marlon Mendes <marlonciriatico@gmail.com>

--- pull_request_metrics/views.py
+++ pull_request_metrics/views.py
@@ -1,12 +1,24 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from oauth.credentials import get_credentials
+
+from github import Github
+from github import GithubException as GE
 
 username, password = get_credentials()
 
 
 def analyze_pull_requests(request, organization, repository):
-    out = 'organization = {}<br>'.format(organization)
-    out += 'repository = {}<br>'.format(repository)
+
+    repository_url = organization + '/' + repository
+    github = Github(username, password)
+    github_repository = github.get_repo(repository_url)
+    github_pull_requests = github_repository.get_pulls()
+
+    out = '<h1> Pull requests<h1><br>'
+    for pr in github_pull_requests:
+        out += (pr.title + '<br>')
 
     return HttpResponse(out)
