b36dd1a77adb626142378987a6e26d93a188bc92
Amanda <amandabs_contato@hotmail.com>
2018-05-28 19:55:48 -0300

    Update django settings - Add session engine to fix error in staging deploy

--- cardinals/settings/staging.py
+++ cardinals/settings/staging.py
@@ -3,6 +3,7 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['cardinalsbeta.herokuapp.com']
 SECRET_KEY = os.environ['SECRET_KEY']
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
