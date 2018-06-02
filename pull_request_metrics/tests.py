bf281e130ae4742ec7b161898a1574a2411eb06b
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-01 23:56:37 -0300

    Tests for pull request openend time
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Marlon Mendes <marlonciriatico@gmail.com>

--- pull_request_metrics/tests.py
+++ pull_request_metrics/tests.py
@@ -6,7 +6,9 @@ from github import Github, PaginatedList, PullRequest
 
 # Create your tests here.
 
-from .views import get_pull_request_opened_time
+from .views import (get_opened_time_xy_axis,
+                    get_pull_request_opened_time,
+                    get_pull_requests_opened_time,)
 
 class SetUp:
     organization = 'fgacardinals'
@@ -28,7 +30,7 @@ class PullRequestTests(TestCase):
                 return pr
         return None
 
-    def test_pull_request_opened_time(self):
+    def test_pull_request_opened_time_for_closed_pr(self):
         repository = PullRequestTests.github.get_repo(PullRequestTests.repository_url)
 
         pull_requests = repository.get_pulls(state='all')
@@ -41,3 +43,51 @@ class PullRequestTests(TestCase):
 
         self.assertEquals(pr.title, expected_pr_title)
         self.assertEquals(opened_time, expected_opened_time)
+
+    def test_pull_request_opened_time_for_closed_pr(self):
+        repository = PullRequestTests.github.get_repo(PullRequestTests.repository_url)
+
+        pull_requests = repository.get_pulls(state='all')
+
+        expected_pr_title = 'Auto commit: peDBFQDrihx'
+        expected_opened_time = 0
+
+        pr = self.get_pr_by_title(pull_requests, expected_pr_title)
+        opened_time = get_pull_request_opened_time(pr).days
+
+        self.assertEquals(pr.title, expected_pr_title)
+        self.assertIsNone(pr.closed_at)
+        self.assertTrue(opened_time >= expected_opened_time)
+
+    def test_get_pull_requests_opened_time(self):
+        repository = PullRequestTests.github.get_repo(PullRequestTests.repository_url)
+
+        pull_requests = repository.get_pulls(state='all')
+
+        closed_pr_title = 'Auto commit: GfftEZeNfuW'
+        closed_pr_expected_opened_time = 3
+
+        still_opened_pr_title = 'Auto commit: peDBFQDrihx'
+        titles = (closed_pr_title, still_opened_pr_title)
+
+        prs_opened_time = get_pull_requests_opened_time(pull_requests)
+        for index, pr in enumerate(pull_requests):
+            opened_time = prs_opened_time[index].days
+            if pr.title == closed_pr_title:
+                self.assertEquals(opened_time, closed_pr_expected_opened_time)
+            elif pr.title == still_opened_pr_title:
+                self.assertTrue(opened_time >= 0)
+
+    def test_get_opened_time_xy_axis(self):
+        repository = PullRequestTests.github.get_repo(PullRequestTests.repository_url)
+
+        pull_requests = repository.get_pulls(state='all')
+        prs_opened_time = get_pull_requests_opened_time(pull_requests)
+
+        x, y = get_opened_time_xy_axis(prs_opened_time)
+
+        expected_y = [1, 1]
+        self.assertEquals(y, expected_y)
+
+        self.assertEquals(x[0], 3)
+        self.assertTrue(x[1] >= 0)
