039507dff5c637069d0b0ad7728af3c463ec0f40
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2018-06-09 15:50:24 -0300

    Plotting chart commits
    Co-authored-by: Gustavo Duarte Moreira <gustavoduartemoreira@gmail.com>
    Co-authored-by: Matheus Gomes <matheusgomesf@gmail.com>

--- commits_charts/views.py
+++ commits_charts/views.py
@@ -6,29 +6,55 @@ from collections import *
 from datetime import datetime, timedelta
 
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import DatetimeTickFormatter, ColumnDataSource
 from bokeh.embed import components
 
 username, password = get_credentials()
-dates=[]
-all_amount_by_date=[]
-signed_amount_by_date=[]
+ 
 
-
-
-def commits_charts(commits_charts):
+def get_multi_line_plot(dates, all_amount_by_date, signed_amount_by_date):
+    plot = figure(plot_width=800, plot_height=500)
     
-    username, password = get_credentials()
-    g = Github(username, password)
-    user = g.get_user()
-    org = g.get_organization("fga-gpp-mds")
-    repos = user.get_repos()
-    repo = org.get_repo("2018.1-Cardinals")
-  
+    data = {'xs': [dates, dates],
+            'ys': [all_amount_by_date, signed_amount_by_date],
+            'labels': ['Individual','Pareado'],
+            'color': ['red','green']}
+    source = ColumnDataSource(data)
+    plot.multi_line(xs='xs', ys='ys', legend='labels',color= 'color', source=source)
+    """ 
+    plot.multi_line(xs=[dates,dates], ys=[all_amount_by_date, signed_amount_by_date],
+             legend=["Commits individuais","Commits pareados"],
+             color=['red','green'])
+     """
+    
+    plot.xaxis.formatter=DatetimeTickFormatter(
+        hours=["%d %B %Y"],
+        days=["%d %B %Y"],
+        months=["%d %B %Y"],
+        years=["%d %B %Y"],
+    )
+    plot.xaxis.axis_label = 'Data do Commit'
+    plot.yaxis.axis_label = 'Commits'
+    """ plot.xaxis.ticker = x_ticks """
+
+    plot.title.text = 'Commits Pareados X Commits Individuais'
+    plot.title.align = 'center'
+    plot.title.text_font_size = '20pt'
+
+    return plot
+
+
+def analyze_commits_charts(request, organization, repository):
+
+    repository_url = organization + '/' + repository
+    github = Github(username, password)
+    repository = github.get_repo(repository_url)
+    
 
     all_commit_count = defaultdict(list)
     signed_commit_count = Counter()
 
-    for commit in repo.get_commits():
+    for commit in repository.get_commits():
         real_date = commit.commit.author.date - timedelta(hours=2)
         all_commit_count[real_date.date()].append(commit.commit)
 #     for saved_commit in all_commit_count[real_date.date()]:
@@ -52,42 +78,12 @@ def commits_charts(commits_charts):
     signed_commit_count = sorted(signed_commit_count.items())
     signed_amount_by_date = [x[1] for x in signed_commit_count]
 
-
-    return (dates, all_amount_by_date, signed_amount_by_date)
-    
-
-def get_multi_line_plot(commits_charts):
-    plot = figure(plot_width=800, plot_height=500)
-
-    dates = commits_charts(commits_charts)
-    all_amount_by_date = commits_charts(commits_charts)
-    signed_amount_by_date = commits_charts(commits_charts)
-    
-    plot.multi_line(xs=[dates,dates], ys= [all_amount_by_date,signed_amount_by_date],
-             color=['red','green'])
-    
-    plot.xaxis.axis_label = 'Data do Commit'
-    plot.yaxis.axis_label = 'Commits'
-    """ plot.xaxis.ticker = x_ticks """
-
-    plot.title.text = 'Commits Pareados X Commits Individuais'
-    plot.title.align = 'center'
-    plot.title.text_font_size = '20pt'
-
-    return plot
-
-
-def analyze_commits_charts(request, organization, repository):
-
-    repository_url = organization + '/' + repository
-    github = Github(username, password)
-    repository = github.get_repo(repository_url)
-    plot = get_multi_line_plot(commits_charts)
+    plot = get_multi_line_plot(dates, all_amount_by_date, signed_amount_by_date)
     script, div = components(plot)
 
     context = {'script': script,
                'div': div,
                'repository': repository_url}
 
-    return render(request, 'bokeh_graph.html', context)
+    return render(request, 'commits_charts.html', context)
 
