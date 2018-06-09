b61a31f70c7798716d4ca905e8eb7b12c777d370
Matheus Gomes <matheusgomesf01@gmail.com>
2018-06-09 12:54:46 -0300

    building charts time open issues
    Co-authored-by: Matheus Gomes <matheusgomesf@gmail.com>
    Co-authored-by: Gustavo Duarte Moreira <gustavoduartemoreira@gmail.com>

--- time_issue/views.py
+++ time_issue/views.py
@@ -14,15 +14,29 @@ from bokeh.embed import components
 username, password = get_credentials()
 
 # Create your views here.
-def timeIssue(request):
-    username, password = get_credentials()
-    g = Github(username, password)
-    user = g.get_user()
-    org = g.get_organization("fga-gpp-mds")
-    repos = user.get_repos()
-    repo = org.get_repo("2018.1-Cardinals")
-   
-    issues = repo.get_issues(state="all")
+
+def get_multi_line_plot(days, amount):
+    plot = figure(plot_width=800, plot_height=500)
+
+    plot.vbar(x=amount, width=0.5, bottom=0, top=days, color="#CAB2D6")
+    
+    plot.xaxis.axis_label = 'Quantidade de issues'
+    plot.yaxis.axis_label = 'Tempo que a issue ficou aberta (dias)'
+    """ plot.xaxis.ticker = x_ticks """
+
+    plot.title.text = 'Período em que as issues ficam abertas'
+    plot.title.align = 'center'
+    plot.title.text_font_size = '20pt'
+
+    return plot
+
+def analyze_issue_graph(request, organization, repository):
+
+    repository_url = organization + '/' + repository
+    github = Github(username, password)
+    repository = github.get_repo(repository_url)
+    
+    issues = repository.get_issues(state="all")
     all_issues = []
     time_open = Counter()
 
@@ -47,33 +61,14 @@ def timeIssue(request):
     print(amount)
 
     mean = sum(amount)/len(amount)  
-    return render({'repo' : days, 'time' : time_open})
 
-def get_multi_line_plot(timeIssue):
-    plot = figure(plot_width=800, plot_height=500)
-
-    plot.multi_line(xs=[[1, 2, 3], [2, 3, 4]], ys=[[6, 7, 2], [4, 5, 7]],
-             color=['red','green'])
-    
-    plot.xaxis.axis_label = 'Data do Commit'
-    plot.yaxis.axis_label = 'Commits'
-    """ plot.xaxis.ticker = x_ticks """
-
-    plot.title.text = 'Commits Pareados X Commits Individuais'
-    plot.title.align = 'center'
-    plot.title.text_font_size = '20pt'
-
-    return plot
-def analyze_issue_graph(request, organization, repository):
-
-    repository_url = organization + '/' + repository
-    github = Github(username, password)
-    repository = github.get_repo(repository_url)
-    plot = get_multi_line_plot(timeIssue)
+    plot = get_multi_line_plot(days, amount)
     script, div = components(plot)
+
 
     context = {'script': script,
                'div': div,
                'repository': repository_url}
 
-    return render(request, 'bokeh_graph.html', context)
+
+    return render(request, 'timeissue.html', context)
