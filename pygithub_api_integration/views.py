721453848c26289a37e97f6e7f37321a597d5ac7
mateusas3s <mateusaugusto-2009@hotmail.com>
2018-06-09 12:53:14 -0300

    fixing NoneType and timeout errors
    Co-authored-by: João Pedro <jpmartins201@live.com>

--- pygithub_api_integration/views.py
+++ pygithub_api_integration/views.py
@@ -3,8 +3,10 @@ from github import GithubException as GE
 from cardinals.views import getRepository
 from pygithub_api_integration.models import Repository
 from pygithub_api_integration.models import Contributor
+from pygithub_api_integration.models import Issue
 # from pygithub_api_integration.models import Commit
 from django.contrib import messages
+import socket
 from . import constants
 
 
@@ -16,13 +18,9 @@ def getRepoInfo(request):
     repo_name = getRepository(request)
     save_repository_name_in_session(request, repo_name)
 
-    repo_request = None
-
     try:
 
-        while repo_request is None:
-            repo_request = Repository.requestRepo(repo_name)
-
+        repo_request = Repository.requestRepo(repo_name)
         repo = Repository.saveRepo(repo_request)
 
         contributors_request = Contributor.requestContributors(repo_request)
@@ -32,6 +30,9 @@ def getRepoInfo(request):
 
         # commit_request = Commit.requestCommit(repo_request)
         # Commit.saveCommit(commit_request, repo, contributors)
+
+        issue_request = Issue.requestIssues(repo_request)
+        Issue.saveIssues(issue_request, repo)
 
         context = {"repo": repo, "contributors": contributors}
 
@@ -43,4 +44,12 @@ def getRepoInfo(request):
             messages.ERROR,
             constants.INVALID_REPOSITORY_MESSAGE,
         )
+
+    except socket.timeout:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            constants.TIMEOUT_MESSAGE,
+        )
+
         return redirect('index')
