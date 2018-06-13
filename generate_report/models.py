c229f69d6bad0b1433ede695b6fe25f9f9413616
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-06-12 22:09:59 -0300

    creating generate_report model

--- generate_report/models.py
+++ generate_report/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from pygithub_api_integration.models import Repository
 
-# Create your models here.
+
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    repo_name = models.CharField(max_length=255, null=False)
+    chart_pr = models.ImageField()
+    chart_commit = models.ImageField()
+    chart_issue = models.ImageField()
+    repository = models.ForeignKey('Repository',
+                                   on_delete=models.CASCADE)
+
+
+class Commiter(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=255, null=True)
+    login = models.CharField(max_length=255)
+    score = models.DecimalField(max_digits=9, decimal_places=2, null=True)
+    report = models.ManyToManyField(Report)
+
+
+class Doc(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    status = models.BooleanField(max_length=255)
+    report = models.ManyToManyField(Report)
