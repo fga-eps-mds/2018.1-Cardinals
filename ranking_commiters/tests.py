8d73ef4bbf83cf5ae51fa01607be35a010e44e27
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-23 01:38:40 -0300

    Testing ranking commiters logins
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- ranking_commiters/tests.py
+++ ranking_commiters/tests.py
@@ -5,13 +5,21 @@ from django.test import TestCase
 #from .models import Contributor
 
 
-class RankingCommitsTests(SetupTestCases):
+class RankingCommitsTests(TestCase):
+    organization = 'fgacardinals'
+    repository = 'test_commiters'
+    url_name = 'ranking_commiters'
+    url = reverse(url_name,
+                  kwargs={'organization': organization,
+                          'repository': repository})
 
     def test_get_commiters_name(self):
-        url = reverse('pull_requests',
-                      kwargs={'organization': SetupTestCases.organization,
-                              'repository': SetupTestCases.repo_name})
-        response = self.client.get(url)
 
-        print("TESTE")
-        print(response)
+        response = self.client.get(RankingCommitsTests.url)
+        self.assertEquals(response.status_code, 200)
+
+        commiters_context = 'ranking_commiters'
+        commiters = response.context[commiters_context]
+        contributors_login = [contributor.login for contributor in commiters]
+        expected_logins = ['lorryaze', 'marlonbymendes']
+        self.assertEquals(contributors_login, expected_logins)
