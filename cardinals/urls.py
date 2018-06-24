2356cc16355a13c81c89516bc7ddf77cf2ad5b3f
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-24 00:36:18 -0300

    Organization page, linking user's organization to repositories
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Co-authored-by: Mateus Augusto <mateusaugusto-2009@hotmail.com>
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- cardinals/urls.py
+++ cardinals/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include
 from django.urls import path
 from cardinals import views
 from django.contrib.auth import views as auth_views
-from login_github.views import home
+
+import login_github.views as login_views
 
 
 urlpatterns = [
@@ -13,7 +14,8 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    path('home/', home, name='home'),
+    path('home/', login_views.home, name='home'),
+    path('<str:login>/', login_views.organization, name='organization'),
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
     path('oauth/', include('social_django.urls', namespace='social')),  # <--
--- login_github/views.py
+++ login_github/views.py
@@ -32,19 +32,45 @@ def get_access_token(request):
     return access_token
 
 
+def save_organizations_in_session(request, orgs):
+    data = dict()
+
+    for org in orgs:
+        repos = org.get_repos(type='all')
+        repo_names = [repo.full_name for repo in repos]
+        data[org.login] = repo_names
+
+    request.session['organizations'] = data
+
+
 def home(request):
 
     context = {}
     if request.user.is_authenticated:
         g = github_singleton.get_github(request)
         user = g.get_user()
-        repo_names = [(repo.full_name, repo.html_url)
+
+        repo_names = [repo.full_name
                       for repo in user.get_repos(type='all')]
 
+
         orgs = user.get_orgs()
-        orgs_names = [(org.login, org.html_url) for org in orgs]
+        save_organizations_in_session(request, orgs)
+
+        orgs_logins = [org.login for org in orgs]
 
         context = {'repositories': repo_names,
-                   'organizations': orgs_names}
+                   'organizations': orgs_logins}
 
     return render(request, 'home_login.html', context=context)
+
+def get_repos_from_organization(request, login):
+    organizations = request.session.get('organizations', None)
+    repositories = organizations.get(login, None)
+    return repositories
+
+def organization(request, login):
+    repositories = get_repos_from_organization(request, login)
+    context = {'login': login,
+               'repositories': repositories}
+    return render(request, 'organizations.html', context=context)
