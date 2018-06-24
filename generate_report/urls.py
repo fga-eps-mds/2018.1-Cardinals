8d1ebdf6f10a1b143e7ed8b12b513081a1d1e6e6
Mateus Augusto <mateusaugusto-2009@hotmail.com>
2018-06-24 13:00:37 -0300

    refatoring generate_report code

--- generate_report/urls.py
+++ generate_report/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from generate_report import views
 
 urlpatterns = [
-    path('pdfView/<int:repo_id>/', views.pdfView, name='pdfView'),
+    path('pdf_view/<int:repo_id>/', views.pdf_view, name='pdf_view'),
 ]
--- generate_report/views.py
+++ generate_report/views.py
@@ -1,4 +1,5 @@
 import time
+import pdfcrowd
 
 from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib.pagesizes import A4
@@ -12,13 +13,49 @@ from pygithub_api_integration.models import Repository
 from pygithub_api_integration.models import Contributor
 from ranking_commiters.models import Weight
 
-MARGIN_LEFT = 50
-SPACE_VERT = 20
-LINE = 213
-CENTER_PAG = 300
+
+def create_pdf_paragraph(vector, text, size):
+
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+    ptext = '<font size=%d>%s</font>' % (size, text)
+
+    vector.append(Paragraph(ptext, styles["Normal"]))
 
 
-def pdfView(request, repo_id):
+def create_pdf_space(vector):
+    vector.append(Spacer(1, 12))
+
+
+def create_pdf_img(vector, img_obj, image):
+
+    img_obj = Image(image, 6.5 * inch, 3 * inch)
+    vector.append(img_obj)
+
+
+def config_convert_img():
+
+    user_api_pdfcrowd = 'mateusas3s'
+    key_api_pdfcrowd = '7aa4ecdff52221464868419a3f842b00'
+
+    img_config = pdfcrowd.HtmlToImageClient(user_api_pdfcrowd,
+                                            key_api_pdfcrowd)
+
+    # configure the conversion
+    img_config.setOutputFormat('jpg')
+
+    return img_config
+
+
+def convert_html2image(img_obj, path_html, path_jpg):
+
+    # run the conversion and write the result to a file
+    img_obj.convertFileToFile(path_html, path_jpg)
+
+    return path_jpg
+
+
+def pdf_view(request, repo_id):
 
     repo = Repository.objects.get(id=repo_id)
     commiters = Contributor.objects.filter(repository=repo_id)
@@ -26,8 +63,8 @@ def pdfView(request, repo_id):
 
     ranking_commiters = Contributor.getScore(commiters, weight)
 
-    response = HttpResponse(content_type='application/pdf')
     report_name = 'attachment; filename=Relatorio_' + repo.full_name + '.pdf'
+    response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = report_name
 
     pdf = SimpleDocTemplate(response, pagesize=A4)
@@ -35,25 +72,33 @@ def pdfView(request, repo_id):
     Story = []
 
     # nome do repositório
-    styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-    ptext = '<font size=16>%s</font>' % repo.full_name
-
-    Story.append(Paragraph(ptext, styles["Normal"]))
-    Story.append(Spacer(1, 12))
+    create_pdf_paragraph(Story, repo.full_name, 16)
+    create_pdf_space(Story)
 
     # contribuidores
-    styles = getSampleStyleSheet()
-    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
-    ptext = '<font size=12>%s</font>' % "Contribuidores"
-
-    Story.append(Paragraph(ptext, styles["Normal"]))
-    Story.append(Spacer(1, 4))
+    create_pdf_paragraph(Story, "Contribuidores", 12)
+    create_pdf_space(Story)
 
     for c in ranking_commiters:
         contrib = c.name + ' / ' + c.login + ' | ' + str(c.score)
-        ptext = '<font size=10>%s</font>' % contrib
-        Story.append(Paragraph(ptext, styles["Normal"]))
+        create_pdf_paragraph(Story, contrib, 10)
+
+    img_obj = config_convert_img()
+
+    chart_commit = convert_html2image(img_obj,
+                                      'static/images/charts/chart_commit.html',
+                                      'static/images/charts/chart_commit.jpg')
+    create_pdf_img(Story, img_obj, chart_commit)
+
+    chart_pr = convert_html2image(img_obj,
+                                  'static/images/charts/chart_pr.html',
+                                  'static/images/charts/chart_pr.jpg')
+    create_pdf_img(Story, img_obj, chart_pr)
+
+    chart_issue = convert_html2image(img_obj,
+                                     'static/images/charts/chart_issue.html',
+                                     'static/images/charts/chart_issue.jpg')
+    create_pdf_img(Story, img_obj, chart_issue)
 
     pdf.build(Story)
 
