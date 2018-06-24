b98ab8e605188835ea14f349f76567287603f716
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-23 22:54:43 -0300

    Listing user repositories
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Co-authored-by: Mateus Augusto <mateusaugusto-2009@hotmail.com>
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- login_github/views.py
+++ login_github/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
+from github import Github
+
+
+class GithubSingleton:
+    _github = None
+
+    def __init__(self):
+        pass
+
+    def __set_github(self, request):
+        token = get_access_token(request)
+        self._github = Github(token)
+        return self._github
+
+    def get_github(self, request):
+        assert request.user.is_authenticated
+
+        if self._github:
+            return self._github
+        else:
+            return self.__set_github(request)
+
+
+github_singleton = GithubSingleton()
+
+
+def get_access_token(request):
+    user = request.user
+    social = user.social_auth.get(provider='github')
+    access_token = social.extra_data['access_token']
+    return access_token
+
 
 def home(request):
-    print('Hi, im home')
-    return render(request, 'home_login.html')
+
+    context = {}
+    if request.user.is_authenticated:
+        g = github_singleton.get_github(request)
+        user = g.get_user()
+        repo_names = [(repo.full_name, repo.archive_url)
+                      for repo in user.get_repos(type='all')]
+        context = {'header': 'My repositories',
+                   'rows': repo_names}
+    return render(request, 'home_login.html', context=context)
