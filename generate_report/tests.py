e49ebf007215d238d48be6f778477fe70dd8dc4b
Joao Pedro <jpmartins201@live.com>
2018-06-25 07:10:43 -0300

    Adding unit tests for create_pdf_paragraph

--- generate_report/tests.py
+++ generate_report/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import pdfcrowd
 from .views import config_convert_img, convert_html2image
+from .views import create_pdf_paragraph
 
 
 class ReportTests(TestCase):
@@ -9,6 +10,11 @@ class ReportTests(TestCase):
    def setUp(self):
       self.path_html = 'static/images/charts/chart_commit.html'
       self.path_jpg = 'static/images/charts/chart_commit.jpg'
+      self.text = "Cardinals"
+      self.size = 12
+      self.Story = []
+      self.test_list = []
+
 
    def test_config_convert_img(self):
       self.img_test = config_convert_img()
@@ -19,3 +25,8 @@ class ReportTests(TestCase):
       self.img_test = config_convert_img()
       self.assertEqual(convert_html2image(self.img_test, self.path_html, self.path_jpg), self.path_jpg)
       self.assertNotEqual(convert_html2image(self.img_test, self.path_html, self.path_jpg), self.img_test)
+
+   def test_create_pdf_paragraph(self):
+      create_pdf_paragraph(self.Story, self.text, self.size)
+      create_pdf_paragraph(self.test_list, self.text, self.size)
+      self.assertNotEqual(self.Story, self.test_list)
